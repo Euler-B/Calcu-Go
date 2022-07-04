538ea7da782982705e3a79862c4e0a0e86fbd116
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-04 17:23:45 -0400

    refactor code

--- main.go
+++ main.go
@@ -8,33 +8,52 @@ import (
 	"strings"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	operacion := scanner.Text()
-	operador := "r"
-	valores := strings.Split(operacion, operador)
-	
-	operador1, err1 := strconv.Atoi(valores[0])
-	operador2, err2 := strconv.Atoi(valores[1])
+type calc struct{}
 
-	if err1 != nil || err2 != nil{
-		fmt.Println("El dato ingresado es incorrecto")
-	} 
+func (calc) operate (entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
 	
 	switch operador {
+
 	case "+":
 		fmt.Println(operador1 + operador2)
+		return operador1 + operador2
+
 	case "-":
 		fmt.Println(operador1 - operador2)
+		return operador1 - operador2
+		
 	case "*":
 		fmt.Println(operador1 * operador2)
+		return operador1 * operador2
+
 	case "/":
 		fmt.Println(operador1 / operador2)
+		return operador1 / operador2
 	default:
 		fmt.Println("El operador no esta soportado")
+		return 0
 	}
-	
-	
+
+}
+
+func parsear (entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	entrada := leerEntrada()
+	operador := leerEntrada()
+	c := calc{}
+	fmt.Println(c.operate(entrada, operador))
 
 }
