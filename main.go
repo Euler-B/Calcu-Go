8fcef73ab6d083a77c6729827864f3c7d8a1e344
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-04 16:47:11 -0400

    added the rest of the arithmetic operations

--- main.go
+++ main.go
@@ -12,16 +12,27 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	operacion := scanner.Text()
-	//fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
-	//fmt.Println(valores)
+	operador := "r"
+	valores := strings.Split(operacion, operador)
+	
 	operador1, err1 := strconv.Atoi(valores[0])
 	operador2, err2 := strconv.Atoi(valores[1])
 
 	if err1 != nil || err2 != nil{
 		fmt.Println("El dato ingresado es incorrecto")
-	} else {
+	} 
+	
+	switch operador {
+	case "+":
 		fmt.Println(operador1 + operador2)
+	case "-":
+		fmt.Println(operador1 - operador2)
+	case "*":
+		fmt.Println(operador1 * operador2)
+	case "/":
+		fmt.Println(operador1 / operador2)
+	default:
+		fmt.Println("El operador no esta soportado")
 	}
 	
 	
